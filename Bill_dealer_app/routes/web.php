b582f2224141402c2080cc6d28d0b62ebd8ac9ac
pascagabriela <daniela.gabi10@yahoo.com>
2022-08-03 20:13:21 +0300

    Daniela's commit

--- Bill_dealer_app/routes/web.php
+++ Bill_dealer_app/routes/web.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/company', function () {
+    return view('auth.company');
+});
+
 Route::get('/home', function () {
     return view('home');
 })->middleware('auth');
+
