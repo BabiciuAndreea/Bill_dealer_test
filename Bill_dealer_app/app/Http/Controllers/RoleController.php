0bdd803fa9f6df7d27d415779f75c631f8dd7fa7
Babiciu Andreea <79803807+BabiciuAndreea@users.noreply.github.com>
2022-08-24 21:55:48 +0300

    update

--- Bill_dealer_app/app/Http/Controllers/RoleController.php
+++ Bill_dealer_app/app/Http/Controllers/RoleController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
     public function setRole(){
+
+   $products = Product::latest()->paginate(3);
+
         if(Auth::check()){
             if(Auth::user()->role == 'admin'){
-                return view('dashboard.index');
+                return view('dashboard.index',compact('products'));
             }else{
                 return view('users.user');
             }
