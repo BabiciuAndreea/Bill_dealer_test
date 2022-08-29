6e3016ce159f8b917b2a102f09931706179ceb32
pascagabriela <daniela.gabi10@yahoo.com>
2022-08-29 13:05:47 +0300

    commit

--- Bill_dealer_app/database/migrations/2022_08_26_093859_add_settings_id_to_users_table.php
+++ Bill_dealer_app/database/migrations/2022_08_26_093859_add_settings_id_to_users_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('settings_id');
-            $table->string('pdf_template');
+            $table->string('settings_id')->nullable();
         });
     }
 
