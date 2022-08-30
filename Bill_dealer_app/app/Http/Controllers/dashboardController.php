e8e9687d3b5e6162795bffd573d40c42cb237024
PataceanMaria <79440130+PataceanMaria@users.noreply.github.com>
2022-08-30 12:27:39 +0300

    cache for bnr added

--- Bill_dealer_app/app/Http/Controllers/dashboardController.php
+++ Bill_dealer_app/app/Http/Controllers/dashboardController.php
@@ -6,15 +6,18 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Bnrcourse;
+use Illuminate\Support\Facades\Cache;
 
 class dashboardController extends Controller
 {
     public function index()
     {
         $products = Product::latest()->paginate(3);
-        $curs=Bnrcourse::getCursFromBNR();
+        // $curs=Bnrcourse::getCursFromBNR();
         // dd($curs);
         $coins = ['EUR', 'USD', 'XAU', 'HUF', 'RUB,'];
+        $curs = Bnrcourse::getCursFromBNR();
+        
         return view('dashboard.index',compact('products', 'curs', 'coins'))
 
             ->with('i', (request()->input('page', 1) - 1) * 5);
